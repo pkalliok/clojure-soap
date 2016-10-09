3c0bf7ea28f4c51fb892970b4c8e7fe4a42c2701
Panu Kalliokoski <panu.kalliokoski@solita.fi>
2016-10-09 15:35:27 +0300

    Fix lein new boilerplate in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
-(defproject clojure-soap "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+(defproject fi.sange.atehwa/clojure-soap "0.1.0"
+  :description "Very simple bindings from Clojure to JAX-WS."
+  :url "https://github.com/pkalliok/clojure-soap"
+  :license {:name "The unlicense" :url "https://unlicense.org/UNLICENSE"}
   :dependencies [[org.clojure/clojure "1.8.0"]])
